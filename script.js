4d6f18451568e83994bddc671a35b56a3912ea5a
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-18 11:05:13 +0100

    Completed the Lightbulb task

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Write your code here
+
+const allBulbsArray = document.querySelectorAll(".lightbulb");
+
+// console.log(allBulbsArray);
+
+function onClick(event) {
+  //   console.log(event.target);
+  let clickedBulb = event.target;
+  if (clickedBulb.className.includes("active")) {
+    clickedBulb.className = "item lightbulb";
+  } else {
+    clickedBulb.className = "item lightbulb active";
+  }
+}
+
+allBulbsArray.forEach((bulb) => {
+  bulb.addEventListener("click", onClick);
+});
